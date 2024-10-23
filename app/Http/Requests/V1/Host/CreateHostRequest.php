c7fcc874b3bb627937af2eb757c848c9bc1d138a
Андрей Евдокимов <aevsergeevich@gmail.com>
2024-10-23 21:07:52 +0400

    Добавлена валидация для проекта и хоста

---
+++ app/Http/Requests/V1/Host/CreateHostRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\V1\Host;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateHostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'project_id' => ['required', 'exists:projects,id'],
+            'url' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
